0f989503ec1f1888faa5832aad4204c801dc4866
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2023-01-27 21:52:20 +0100

    resized layout and added border for mobile version

--- script-abdelkrim.js
+++ script-abdelkrim.js
@@ -12,16 +12,19 @@ function makeElementActive(element){
   console.log('inside the make element active')
   sideBarItems.forEach((item,index)=>{
     if(item===element) {
-       element.querySelector('.sidebar-link').classList.add('active')
+       element.querySelector('.sidebar-link').classList.add('activated')
        console.log(element.querySelector('.sidebar-link'));
-      //  element.querySelector('.sidebar-icon').classList.add('active');
-      //  element.querySelector('.sidebar-link-text').classList.add('active');
+       console.log(element.querySelector('.sidebar-link').querySelector('.sidebar-icon'))
+       console.log(element.querySelector('.sidebar-link-text'))
+       element.querySelector('.sidebar-link').querySelector('.sidebar-icon').classList.add('activated');
+       element.querySelector('.sidebar-link-text').classList.add('activated');
+
       
     }
     else{
-      element.querySelector('.sidebar-link').classList.remove('active');
-      //  element.querySelector('.sidebar-icon').classList.remove('active');
-      //  element.querySelector('.sidebar-link-text').classList.remove('active');
+      element.querySelector('.sidebar-link').classList.remove('activated');
+       element.querySelector('.sidebar-link').querySelector('.sidebar-icon').classList.remove('activated');
+       element.querySelector('.sidebar-link-text').classList.remove('activated');
     }
   })
 }
